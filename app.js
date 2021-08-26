5bcbe5d9bd099d57b2ac3975a5763610043555e2
Victor Plushko <vicplushko33@gmail.com>
2021-08-26 12:32:40 -0400

    requires file system module, uses writeFileSync and appendFileSync to create a .txt file and append something to the file

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-false.writeFileSync('notes.txt', 'This file was created by Node.js!')
+const fs = require('fs')
+
+
+fs.writeFileSync('notes.txt', 'My name is Victor and I am learning Node.js!')
+
+fs.appendFileSync('notes.txt', ' I am excited to learn node.js')
